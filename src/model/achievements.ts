6901cfcc579d1bd0e6a16d2f8d655758e07c2549
UniTaeMin <onnu0802@naver.com>
2020-06-30 08:51:52 +0900

    Add achievements model

---
+++ src/model/achievements.ts
@@ -0,0 +1,138 @@
+import { model, Schema, Model, Document } from "mongoose";
+import * as jwt from "jsonwebtoken";
+import * as moment from "moment";
+import User from "./user";
+const achievementsSchema = new Schema({
+  owner: {
+    type: String,
+    required: true,
+  },
+  successNumber: {
+    type: Number,
+    required: true,
+  },
+  list: {
+    type: Array,
+    required: true,
+  },
+});
+export interface IAchievements extends Document {
+  owner: string;
+  successNumber: number;
+  list: Array<any>;
+}
+export interface Result {
+  mes: string;
+  success: boolean;
+}
+export interface AchievementsDocument extends Document, IAchievements {}
+export interface IAchievementsDocument extends Model<AchievementsDocument> {
+  create();
+  get();
+}
+achievementsSchema.statics.create = function (email: string): Promise<any> {
+  return new Promise(async function (resolve, reject) {
+    try {
+      const achievements: any = new Achievements({
+        owner: email,
+        successNumber: 0,
+        list: [
+          {
+            category: "수면 시간",
+            data: [
+              {
+                date: 2,
+                hours: 7,
+                title: "2일 연속 7시간 이상 수면",
+                value: false,
+              },
+              {
+                date: 4,
+                hours: 7,
+                title: "4일 연속 7시간 이상 수면",
+                value: false,
+              },
+              {
+                date: 6,
+                hours: 7,
+                title: "6일 연속 7시간 이상 수면",
+                value: false,
+              },
+              {
+                date: 8,
+                hours: 7,
+                title: "8일 연속 7시간 이상 수면",
+                value: false,
+              },
+              {
+                date: 10,
+                hours: 7,
+                title: "10일 연속 7시간 이상 수면",
+                value: false,
+              },
+              {
+                date: 12,
+                hours: 7,
+                title: "12일 연속 7시간 이상 수면",
+                value: false,
+              },
+              {
+                date: 14,
+                hours: 7,
+                title: "14일 연속 7시간 이상 수면",
+                value: false,
+              },
+            ],
+          },
+        ],
+      });
+      achievements.save().then((data) => {
+        console.log("데이터 생성", data);
+        return resolve({
+          success: true,
+          mes: "업적을 생성 하였습니다",
+        });
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+//전체 출력
+achievementsSchema.statics.get = function (email: string): Promise<any> {
+  return new Promise(async function (resolve, reject) {
+    try {
+      //이메일에 해당되는 업적 전체를 가져오면 됨
+      return resolve({
+        success: true,
+        mes: "성공적으로 호출 하였습니다",
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+achievementsSchema.statics.update = function (
+  category: string,
+  title: string,
+  email: string,
+  state: boolean = true
+): Promise<any> {
+  return new Promise(async function (resolve, reject) {
+    try {
+      //해당되는 업적의 이름을 통해 그 업적을 업데이트 시키면 됨
+      return resolve({
+        success: true,
+        mes: "업데이트를 성공 하였습니다",
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+const Achievements: Model<IAchievementsDocument> = model(
+  "achievements",
+  achievementsSchema
+) as IAchievementsDocument;
+export default Achievements;
