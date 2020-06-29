e3b4586f3f9d95f32479bb892c43d44e5c0807df
UniTaeMin <onnu0802@naver.com>
2020-06-30 01:30:32 +0900

    Update Swagger

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -21,13 +21,13 @@ swaggerSpec.definitions.ResponseSignin = require("../Swagger/ResponseSignin.mode
 swaggerSpec.definitions.ResponseSignup = require("../Swagger/ResponseSignup.model.json");
 swaggerSpec.definitions.RequestSleepUpdate = require("../Swagger/RequestSleepUpdate.model.json");
 swaggerSpec.definitions.ResponseSleepUpdate = require("../Swagger/ResponseSleepUpdate.model.json");
-
 swaggerSpec.definitions.RequestInfo = require("../Swagger/RequestInfo.model.json");
 swaggerSpec.definitions.ResponseInfo = require("../Swagger/ResponseInfo.model.json");
-
 swaggerSpec.definitions.RequestUpdateInfo = require("../Swagger/RequestUpdateInfo.model.json");
 swaggerSpec.definitions.ResponseUpdateInfo = require("../Swagger/ResponseUpdateInfo.model.json");
-
+swaggerSpec.definitions.ResponseRefresh = require("../Swagger/ResponseRefresh.model.json");
+swaggerSpec.definitions.RequestGetSleepTime = require("../Swagger/RequestGetSleepTime.model.json");
+swaggerSpec.definitions.ResponseGetSleepTime = require("../Swagger/ResponseGetSleepTime.model.json");
 swaggerSpec.definitions.Token = require("../Swagger/Token.model.json");
 
 export { swaggerSpec };
